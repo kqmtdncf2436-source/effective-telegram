da1d245210050ba9ced130df5d59fdf594788aff
kqmtdncf2436-source <kqmtdncf2436@gmail.com>
2026-07-26 23:57:10 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
-print("FinalK Bot Started")
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+from config import BOT_TOKEN
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "سلام 🌸\n\nبه ربات پخش نهایی خوش اومدی."
+    )
+
+app = Application.builder().token(BOT_TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
