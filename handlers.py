c31fddb95669e7def22084f1ee5c8defaf03b3df
kqmtdncf2436-source <kqmtdncf2436@gmail.com>
2026-07-27 04:04:01 +0330

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,34 +1,55 @@
+import asyncio
+
 from telegram import Update
 from telegram.ext import ContextTypes
 
 from texts import *
+from codes import (
+    code_exists,
+    user_used,
+    mark_used,
+    get_code,
+)
+
 
 waiting_for_code = {}
 
-main_menu = [
-    ["📦 دریافت سفارش", "💎 خدمات و قیمت‌ها"],
-    ["📖 راهنما", "💬 پشتیبانی"],
-]
 
-
-async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def button_handler(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+):
 
     text = update.message.text
     user_id = update.effective_user.id
 
+
     if text == "💬 پشتیبانی":
-        await update.message.reply_text(SUPPORT)
+
+        await update.message.reply_text(
+            SUPPORT
+        )
         return
+
 
     if text == "💎 خدمات و قیمت‌ها":
-        await update.message.reply_text(SERVICES)
+
+        await update.message.reply_text(
+            SERVICES
+        )
         return
+
 
     if text == "📖 راهنما":
-        await update.message.reply_text(HELP)
+
+        await update.message.reply_text(
+            HELP
+        )
         return
 
+
     if text == "📦 دریافت سفارش":
+
         waiting_for_code[user_id] = True
 
         await update.message.reply_text(
@@ -36,20 +57,56 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
         )
         return
 
+
+
     if waiting_for_code.get(user_id):
 
         code = text.upper()
 
-        await update.message.reply_text(
-            f"کد دریافت شد:\n\n{code}\n\n(مرحله بعد فایل ارسال خواهد شد)"
+
+        if not code_exists(code):
+
+            await update.message.reply_text(
+                INVALID_CODE
+            )
+
+            return
+
+
+
+        if user_used(code, user_id):
+
+            await update.message.reply_text(
+                USED_CODE
+            )
+
+            return
+
+
+
+        mark_used(
+            code,
+            user_id
         )
 
-        waiting_for_code.pop(user_id)
+
+        await update.message.reply_text(
+            PREPARING
+        )
+
+
+        await asyncio.sleep(2)
+
+
+        await update.message.reply_text(
+            SENDING
+        )
+
 
         return
 
+
+
     await update.message.reply_text(
-        "🔔 ابتدا روی گزینه\n\n"
-        "📦 دریافت سفارش\n\n"
-        "بزنید."
+        UNKNOWN_MESSAGE
     )
