2cf1f982c76dae418ac5d5fab375386de8782b9f
kqmtdncf2436-source <kqmtdncf2436@gmail.com>
2026-07-27 04:02:11 +0330

    Update codes.py

--- codes.py
+++ codes.py
@@ -2,16 +2,73 @@ import json
 import os
 
 
+CODES_FILE = "codes.json"
+
+
 def load_codes():
 
-    if os.path.exists("codes.json"):
+    if not os.path.exists(CODES_FILE):
+        return {}
 
-        with open(
-            "codes.json",
-            "r",
-            encoding="utf-8"
-        ) as f:
+    with open(
+        CODES_FILE,
+        "r",
+        encoding="utf-8"
+    ) as f:
 
-            return json.load(f)
+        return json.load(f)
 
-    return {}
+
+def save_codes(codes):
+
+    with open(
+        CODES_FILE,
+        "w",
+        encoding="utf-8"
+    ) as f:
+
+        json.dump(
+            codes,
+            f,
+            ensure_ascii=False,
+            indent=4
+        )
+
+
+def code_exists(code):
+
+    codes = load_codes()
+
+    return code in codes
+
+
+def get_code(code):
+
+    codes = load_codes()
+
+    return codes.get(code)
+
+
+def user_used(code, user_id):
+
+    codes = load_codes()
+
+    if code not in codes:
+        return False
+
+    return str(user_id) in codes[code]["used"]
+
+
+def mark_used(code, user_id):
+
+    codes = load_codes()
+
+    if code not in codes:
+        return
+
+    uid = str(user_id)
+
+    if uid not in codes[code]["used"]:
+        codes[code]["used"].append(uid)
+
+    save_codes(codes)
